7fe79b2592685512a03cb19fbe9568282b5ef494
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-03 20:46:03 -0500

    fx to wipe dom content (except header)

--- src/ui/dom.js
+++ src/ui/dom.js
@@ -1,1 +1,13 @@
-export default function wipeDomContent() {}
+export default function wipeDomContent() {
+  const parentContainer = document.querySelector('.content');
+  const { children } = parentContainer;
+  const childArray = [...children];
+
+  // will either need to change home-content or add every page here because each page
+  // will be named x-content to style them differently
+  childArray.forEach((child) => {
+    if (child.classList[0] === 'home-content') {
+      parentContainer.removeChild(child);
+    }
+  });
+}
