d01407957a31497588b445206b9d1b6e5080bb84
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-04 15:02:10 -0500

    add fx to build desktop nav

---
+++ src/ui/desktop-nav-menu.js
@@ -0,0 +1,34 @@
+import buildNavLinks from './nav-links';
+
+function checkIfDesktopHeaderExists() {
+  if (document.querySelector('.desktop-header-right')) return true;
+  return false;
+}
+
+function checkIfMobileHeaderExists(headerDiv, body, mobileNav) {
+  const mobileHeader = document.querySelector('.mobile-header-right');
+
+  if (mobileHeader) {
+    headerDiv.removeChild(mobileHeader);
+    body.removeChild(mobileNav);
+  }
+}
+
+export default function buildDesktopNavMenu() {
+  // if header nav exists when being called be a resize event, just return
+  const headerCheck = checkIfDesktopHeaderExists();
+  if (headerCheck) return;
+
+  const headerDiv = document.querySelector('.header');
+  const body = document.querySelector('.content');
+  const mobileNav = document.querySelector('.mobile-nav');
+
+  // if mobile header exists while resizing window, remove from window
+  checkIfMobileHeaderExists(headerDiv, body, mobileNav);
+
+  const headerRight = document.createElement('div');
+  headerRight.classList.add('desktop-header-right');
+  headerDiv.appendChild(headerRight);
+
+  buildNavLinks(headerRight);
+}
