a11d9e8415b39e0fb0fb8842c4ae6506f855e55d
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-03 20:48:05 -0500

    add load home page on site load

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import createHeader from './ui/header';
 import './assests/style.css';
 import checkScreenSizeAdjustment from './events/events';
+import loadHomePageOnLoad from './ui/pages/home';
 
 createHeader();
 checkScreenSizeAdjustment();
+loadHomePageOnLoad();
