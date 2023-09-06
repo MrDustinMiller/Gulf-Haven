c597c9e50bc850885316ba7572c9f107f2af9cb9
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-06 01:05:38 -0500

    started about page, made divs

--- src/ui/call-button.js
+++ src/ui/call-button.js
@@ -5,7 +5,7 @@ export default function buildCallToBookButton(titleAndWelcomeDiv) {
 
   const callNowButtonText = document.createElement('a');
   callNowButtonText.classList.add('call-now-btn-text');
-  callNowButtonText.setAttribute('href', 'tel:228-863-9096');
+  callNowButtonText.setAttribute('href', 'tel:2288639096');
   callNowButtonText.setAttribute('target', '_parent');
   callNowButtonText.textContent = 'Call now to reserve!';
   callNowButton.appendChild(callNowButtonText);
--- src/ui/pages/about.js
+++ src/ui/pages/about.js
@@ -1,1 +1,36 @@
-export default function loadAboutPage() {}
+function buildAboutDivs() {
+  const content = document.querySelector('.about-content');
+
+  const pictureDivOne = document.createElement('div');
+  pictureDivOne.classList.add('picture-div-one');
+  pictureDivOne.classList.add('about-div');
+  content.appendChild(pictureDivOne);
+
+  const textDivOne = document.createElement('div');
+  textDivOne.classList.add('text-div-one');
+  textDivOne.classList.add('about-div');
+  content.appendChild(textDivOne);
+
+  const pictureDivTwo = document.createElement('div');
+  pictureDivTwo.classList.add('picture-div-two');
+  pictureDivTwo.classList.add('about-div');
+  content.appendChild(pictureDivTwo);
+
+  const textDivTwo = document.createElement('div');
+  textDivTwo.classList.add('text-div-two');
+  textDivTwo.classList.add('about-div');
+  content.appendChild(textDivTwo);
+}
+
+function buildAboutContentDiv() {
+  const main = document.querySelector('.content');
+  const homeContent = document.createElement('div');
+
+  homeContent.classList.add('about-content');
+  main.appendChild(homeContent);
+  buildAboutDivs();
+}
+
+export default function loadAboutPage() {
+  buildAboutContentDiv();
+}
