bad02ab684055d8f221657e865e48e2376c66624
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-09 03:07:13 -0500

    build about page divs

--- src/ui/pages/about.js
+++ src/ui/pages/about.js
@@ -1,25 +1,88 @@
+import gulfHavenSign from '../../assests/images/gulf-haven-sign.jpg';
+import benchShot from '../../assests/images/bench-shot.jpg';
+import beachShotTwo from '../../assests/images/beach-shot-2.jpg';
+
+function buildPictureDivChildrenDivs(
+  pictureDiv,
+  pictureDivClassName,
+  pictureDivChildTopImg,
+  pictureDivChildBottomLeftImg,
+  pictureDivChildBottomRightImg
+) {
+  // top
+  const pictureDivChildTop = document.createElement('div');
+  pictureDivChildTop.classList.add(`${pictureDivClassName}-child-top`);
+  pictureDivChildTop.style.backgroundImage = `url(${pictureDivChildTopImg})`;
+  pictureDivChildTop.classList.add('picture-div-child');
+  pictureDiv.appendChild(pictureDivChildTop);
+
+  // bottom left
+  const pictureDivChildBottomLeft = document.createElement('div');
+  pictureDivChildBottomLeft.classList.add(
+    `${pictureDivClassName}-child-bottom-left`
+  );
+  pictureDivChildBottomLeft.style.backgroundImage = `url(${pictureDivChildBottomLeftImg})`;
+  pictureDivChildBottomLeft.classList.add('picture-div-child');
+  pictureDiv.appendChild(pictureDivChildBottomLeft);
+
+  // bottom right
+  const pictureDivChildBottomRight = document.createElement('div');
+  pictureDivChildBottomRight.classList.add(
+    `${pictureDivClassName}-child-bottom-right`
+  );
+  pictureDivChildBottomRight.style.backgroundImage = `url(${pictureDivChildBottomRightImg})`;
+  pictureDivChildBottomRight.classList.add('picture-div-child');
+  pictureDiv.appendChild(pictureDivChildBottomRight);
+}
+
+function buildPictureDiv(
+  content,
+  pictureDivClassName,
+  pictureDivChildTopImg,
+  pictureDivChildBottomLeftImg,
+  pictureDivChildBottomRightImg
+) {
+  const pictureDiv = document.createElement('div');
+  pictureDiv.classList.add(`${pictureDivClassName}`);
+  pictureDiv.classList.add('about-div');
+  content.appendChild(pictureDiv);
+
+  buildPictureDivChildrenDivs(
+    pictureDiv,
+    pictureDivClassName,
+    pictureDivChildTopImg,
+    pictureDivChildBottomLeftImg,
+    pictureDivChildBottomRightImg
+  );
+}
+
+function buildTextDiv(content, textDivClassName, textContentClassName) {
+  const textDivOne = document.createElement('div');
+  textDivOne.classList.add(`${textDivClassName}`);
+  textDivOne.classList.add('about-div');
+
+  const textDivOneContent = document.createElement('p');
+  textDivOneContent.classList.add(`${textContentClassName}`);
+  textDivOneContent.textContent =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  // textDivOneContent.textContent = `${textContent}`
+  textDivOne.appendChild(textDivOneContent);
+  content.appendChild(textDivOne);
+}
+
 function buildAboutDivs() {
   const content = document.querySelector('.about-content');
 
-  const pictureDivOne = document.createElement('div');
-  pictureDivOne.classList.add('picture-div-one');
-  pictureDivOne.classList.add('about-div');
-  content.appendChild(pictureDivOne);
-
-  const textDivOne = document.createElement('div');
-  textDivOne.classList.add('text-div-one');
-  textDivOne.classList.add('about-div');
-  content.appendChild(textDivOne);
-
-  const pictureDivTwo = document.createElement('div');
-  pictureDivTwo.classList.add('picture-div-two');
-  pictureDivTwo.classList.add('about-div');
-  content.appendChild(pictureDivTwo);
-
-  const textDivTwo = document.createElement('div');
-  textDivTwo.classList.add('text-div-two');
-  textDivTwo.classList.add('about-div');
-  content.appendChild(textDivTwo);
+  buildPictureDiv(
+    content,
+    'picture-div-one',
+    beachShotTwo,
+    gulfHavenSign,
+    benchShot
+  );
+  buildTextDiv(content, 'text-div-one', 'text-div-one-text-content');
+  buildTextDiv(content, 'text-div-two', 'text-div-two-text-content');
+  buildPictureDiv(content, 'picture-div-two');
 }
 
 function buildAboutContentDiv() {
