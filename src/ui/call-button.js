0fceb27c193586f3a756cfd1986120f91fe32f9b
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-09 03:07:00 -0500

    add images for about page (wip)

--- src/ui/call-button.js
+++ src/ui/call-button.js
@@ -5,9 +5,9 @@ export default function buildCallToBookButton(titleAndWelcomeDiv) {
 
   const callNowButtonText = document.createElement('a');
   callNowButtonText.classList.add('call-now-btn-text');
-  callNowButtonText.setAttribute('href', 'tel:12288639096');
-  callNowButtonText.setAttribute('target', '_blank');
-  callNowButtonText.textContent = 'Call now to reserve!';
+  callNowButtonText.setAttribute('href', 'tel:1-228-8639-096');
+  callNowButtonText.setAttribute('target', '_parent');
+  callNowButtonText.textContent = '228-863-3906!';
   callNowButton.appendChild(callNowButtonText);
 
   const callBtnIcon = document.createElement('i');
