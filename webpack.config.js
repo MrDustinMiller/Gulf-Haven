287e4c5e5620b3ead55c76d961ea2dd62560f2d9
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-13 18:38:06 -0500

    add loaders to handle txt files

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.txt$/i,
+        use: 'raw-loader',
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
