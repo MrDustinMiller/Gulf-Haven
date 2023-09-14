01a9e66cbeaa640a0131db412a3e01dc3f44c7f8
MrDustinMiller <dustin.miller@mscoding.org>
2023-09-13 19:41:53 -0500

    testing call button on home page

--- src/ui/call-button.js
+++ src/ui/call-button.js
@@ -1,16 +1,16 @@
 export default function buildCallToBookButton(titleAndWelcomeDiv) {
-  const callNowButton = document.createElement('button');
-  callNowButton.classList.add('call-now-btn');
-  titleAndWelcomeDiv.appendChild(callNowButton);
+  // const callNowButton = document.createElement('button');
+  // callNowButton.classList.add('call-now-btn');
+  // titleAndWelcomeDiv.appendChild(callNowButton);
 
   const callNowButtonText = document.createElement('a');
   callNowButtonText.classList.add('call-now-btn-text');
   callNowButtonText.setAttribute('href', 'tel:1-228-8639-096');
   callNowButtonText.setAttribute('target', '_parent');
   callNowButtonText.textContent = '228-863-3906!';
-  callNowButton.appendChild(callNowButtonText);
+  titleAndWelcomeDiv.appendChild(callNowButtonText);
 
-  const callBtnIcon = document.createElement('i');
-  callBtnIcon.setAttribute('class', 'fa-solid fa-phone-volume');
-  callNowButton.appendChild(callBtnIcon);
+  // const callBtnIcon = document.createElement('i');
+  // callBtnIcon.setAttribute('class', 'fa-solid fa-phone-volume');
+  // titleAndWelcomeDiv.appendChild(callBtnIcon);
 }
